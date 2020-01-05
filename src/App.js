e4ed80b27c93f8af0c901069eece361be6b225f2
Siraj <sirajchokshi@gmail.com>
2020-01-05 11:33:26 -0500

    update src => add link to homepage

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import Filter from "./components/Filter/Filter";
 import Footer from "./components/Footer/Footer";
 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
-shuffleArray(projects);
+
+shuffle(projects);
 
 export default () => (
     <Router>
       <div className="container">
           <header>
-              <h1><a href="https://sirajchokshi.com"><img src="https://i.imgur.com/trdF8yX.jpg" className="header-profile" alt="" /> Siraj Chokshi</a></h1>
+              <h1><img src="https://i.imgur.com/trdF8yX.jpg" className="header-profile" alt="" /> Siraj Chokshi</h1>
+              <a id="back-to-home" href="https://sirajchokshi.com">&larr; Back to main site</a>
+              <div style={{clear: "both"}} />
               <div className="row">
                   <p id="bio">Hi, I’m Siraj. I’m a UX designer, frontend developer and student at UIUC. This website is a collection of my public repositories and open source contributions.</p>
                   <Filter />
